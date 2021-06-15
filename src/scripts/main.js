71f64eeeb1158b643dd4d4ca87de49b22fa17900
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-06-15 15:16:27 +0300

    mb last rework

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const menu = document.querySelector('.btn');
+const mobileNavigation = document.querySelector('.nav');
+
+menu.addEventListener('click', (event) => {
+  mobileNavigation.classList.toggle('nav--active');
+  event.stopImmediatePropagation();
+});
+
+const form = document.querySelector('.feedback__form');
+const inputs = document.querySelectorAll('.feedback__form-input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+});
+
+const formShops = document.querySelector('.form');
+const selects = document.querySelectorAll('.form__city');
+
+formShops.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  selects.forEach((input) => {
+    input.value = '';
+  });
+});
