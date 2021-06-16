d51d9263f83064a6150c285dee9592ef2f74f113
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-06-16 22:01:49 +0300

    rework#2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,37 @@
 'use strict';
 
-const menu = document.querySelector('.btn');
+const menu = document.querySelector('.btn, btn__off');
 const mobileNavigation = document.querySelector('.nav');
+const main = document.querySelector('.main');
+const checkbox = document.querySelector('.toggle--checkbox');
 
-menu.addEventListener('click', (event) => {
+menu.addEventListener('click', () => {
   mobileNavigation.classList.toggle('nav--active');
-  event.stopImmediatePropagation();
 });
+
+main.addEventListener('click', () => {
+  if (mobileNavigation.classList.contains('nav--active')) {
+    mobileNavigation.classList.toggle('nav--active');
+    checkbox.checked = false;
+  }
+});
+
+const links = document.querySelectorAll('.nav__link');
+
+links.forEach((item) =>
+  item.addEventListener('click', () => {
+    if (mobileNavigation.classList.contains('nav--active')) {
+      mobileNavigation.classList.toggle('nav--active');
+      checkbox.checked = false;
+    }
+  })
+);
 
 const form = document.querySelector('.feedback__form');
 const inputs = document.querySelectorAll('.feedback__form-input');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
   inputs.forEach((input) => {
     input.value = '';
@@ -22,8 +41,8 @@ form.addEventListener('submit', (e) => {
 const formShops = document.querySelector('.form');
 const selects = document.querySelectorAll('.form__city');
 
-formShops.addEventListener('submit', (e) => {
-  e.preventDefault();
+formShops.addEventListener('submit', (event) => {
+  event.preventDefault();
 
   selects.forEach((input) => {
     input.value = '';
